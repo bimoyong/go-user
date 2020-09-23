882b4e3c9772e9dcd2cd7536bd79800db484e857
Bi Mo Yong <bi.moyong@gmail.com>
2020-09-23 23:09:33 +0700

    update how to generate token with custom expiry

--- handler/login.go
+++ handler/login.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/micro/go-micro/v2/auth"
+	"github.com/micro/go-micro/v2/auth/token"
+	"github.com/micro/go-micro/v2/auth/token/jwt"
 	"github.com/micro/go-micro/v2/config"
 	log "github.com/micro/go-micro/v2/logger"
 
@@ -15,23 +17,31 @@ import (
 func (u *User) Login(ctx context.Context, req *proto.LoginReq, rsp *proto.LoginRsp) (err error) {
 	log.Debugf("[User] Request: req=[%+v]", req)
 
-	var acc *auth.Account
-	if acc, err = auth.DefaultAuth.Generate(req.Id); err != nil {
+	acc := &auth.Account{ID: req.Id}
+	privKey := config.Get("auth", "private_key").String("")
+	pubKey := config.Get("auth", "public_key").String("")
+	exp := config.Get("auth", "expiry").Int(0)
+
+	prov := jwt.NewTokenProvider(
+		token.WithPrivateKey(privKey),
+		token.WithPublicKey(pubKey),
+	)
+
+	var tk *token.Token
+	if tk, err = prov.Generate(
+		acc,
+		token.WithExpiry(time.Second*time.Duration(exp)),
+	); err != nil {
 		log.Errorf("[User] Login failure!: err=[%s], id=[%s]", err.Error(), req.Id)
 
 		return
 	}
 	log.Infof("[User] Login success: id=[%s] scope=[%+v]", acc.ID, acc.Scopes)
-	exp := config.Get("auth", "expiry").Int(0)
-	token, _ := auth.DefaultAuth.Token(
-		auth.WithToken(acc.Secret),
-		auth.WithExpiry(time.Second*time.Duration(exp)),
-	)
 
-	rsp.Token = token.AccessToken
-	rsp.Refresh = token.RefreshToken
-	rsp.Created = token.Created.Unix()
-	rsp.Expiry = token.Expiry.Unix()
+	rsp.Token = tk.Token
+	// rsp.Refresh = tk.RefreshToken
+	rsp.Created = tk.Created.Unix()
+	rsp.Expiry = tk.Expiry.Unix()
 
 	return
 }
